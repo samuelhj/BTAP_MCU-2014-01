11e80815416c85761fb3481246acfe2e1054b309
Samúel Úlfr Þór Hjaltalín Guðjónsson <samuel@ulfr.net>
2014-01-23 02:41:20 +0000

    License and other stuff fixed…

--- BTAP_MCU/BTAP_MCU.ino
+++ BTAP_MCU/BTAP_MCU.ino
@@ -7,15 +7,28 @@
 // Authors	 	Samúel Úlfr Þór Hjaltalín Guðjónsson
 // 				samuel@ulfr.net
 //
-//              Michael
+//              Michael Hansen
+//				michael@bylur.net
 //
 //
 // Date			2014.1.19 19:21
-// Version		<#version#>
+// Version		version
 // 
 // Copyright	© Samúel Úlfr Þór Hjaltalín Guðjónsson, 2014
-// License		license
-//
+// License
+/*				Copyright 1992-2014 Bifrost the Aurora Project All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE FREEBSD PROJECT ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE FREEBSD PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the authors and should not be interpreted as representing official policies, either expressed or implied, of the FreeBSD Project.
+ 
+
+*/
 // See			ReadMe.txt for references
 //
 
@@ -329,6 +342,7 @@ int sensor_read()
 	 */
 
 	// We need interval timer here for the writing of the temp_external & temp_internal
+	// fixed
 	// also need to implement some error checking on the write of the eeprom
 	// see http://playground.arduino.cc/Code/EEPROMex
 	// ticket #5
